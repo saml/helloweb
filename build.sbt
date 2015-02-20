a1880eb9a3a08b36b3df5dffa509b6038ca3d3df
saml <skynare@gmail.com>
2015-02-20 17:29:27 -0500

    asdf

--- build.sbt
+++ build.sbt
@@ -6,8 +6,11 @@ version := "0.0.0"
 
 scalaVersion := "2.11.4"
 
+mainClass := Some("servlets.Main")
+
 libraryDependencies ++= Seq(
-  "javax.servlet" % "javax.servlet-api" % "3.1.0" % "provided"
+  "javax.servlet" % "javax.servlet-api" % "3.1.0" % "provided",
+  "org.eclipse.jetty" % "jetty-webapp" % "9.3.0.M1"
 )
 
 
--- src/main/scala/servlets.scala
+++ src/main/scala/servlets.scala
@@ -1,15 +1,38 @@
 package servlets
 
-import javax.servlet.annotation.WebServlet
 
 import javax.servlet.http.HttpServlet
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
+import javax.servlet.{ServletContextEvent, ServletContextListener}
 
-@WebServlet(name="Hello", urlPatterns=Array("/*"))
+import org.eclipse.jetty.server.Server
+import org.eclipse.jetty.webapp.WebAppContext
+
 class HelloServlet extends HttpServlet {
   override def doGet(request: HttpServletRequest, response: HttpServletResponse) {
     response.getWriter().write("Hello, world")
+  }
+}
+
+class ServletInitialization extends ServletContextListener {
+  override def contextInitialized(event: ServletContextEvent) = {
+    val context = event.getServletContext
+    val registration = context.addServlet("Hello", classOf[HelloServlet])
+    registration.addMapping("/*")
+  }
+  override def contextDestroyed(event: ServletContextEvent) = {}
+}
+
+class Main extends App {
+  override def main(args: Array[String]): Unit = {
+    val server = new Server(8080)
+    val initialization = new ServletInitialization()
+    val context = new WebAppContext()
+    context.addEventListener(initialization)
+    context.setServer(server)
+    server.setHandler(context)
+    server.start()
   }
 }
 
