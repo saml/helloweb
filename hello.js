7afa9fa15799ad98773a1e6567a9254be8fd6148
saml <skynare@gmail.com>
2015-02-23 08:40:38 -0500

    cluster

--- hello.js
+++ hello.js
@@ -1,6 +1,16 @@
 var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200);
-  res.end('Hello World');
-}).listen(8080, '127.0.0.1');
-console.log('Server running at http://localhost:8080/');
+var cluster = require('cluster');
+var cpus = require('os').cpus().length;
+if (cluster.isMaster) {
+  for (var i = 0; i < cpus; i++) {
+    cluster.fork();
+  }
+  cluster.on('exit', function(worker, code, signal) {
+  });
+} else {
+  http.createServer(function (req, res) {
+    res.writeHead(200);
+    res.end('Hello World');
+  }).listen(8080, '127.0.0.1');
+  console.log('Server running at http://localhost:8080/');
+}
--- test.bash
+++ test.bash
@@ -6,6 +6,9 @@ nim="${NIM:-nim}"
 sbt="${SBT:-sbt}"
 java="${JAVA:-java}"
 
+duration="${1:-10}"
+should_build="${2:-yes}"
+
 run() {
   local msg="$1"
   shift 1
@@ -14,7 +17,7 @@ run() {
   pid="$!"
   sleep 3 #give time for backgrounded server to initialize
   echo "==== $msg ($pid)"
-  "$wrk" -c 100 -d 10 -t 10 "http://localhost:8080/"
+  "$wrk" -c 100 -d $duration -t 10 "http://localhost:8080/"
   kill "$pid"
   sleep 3
 }
@@ -24,9 +27,12 @@ echo "      $node"
 echo "      $nim"
 echo "      $sbt"
 
-# build
-"$nim" -d:release c hello
-"$sbt" assembly
+if [[ "$should_build" = "yes" ]]
+then
+  # build
+  "$nim" -d:release c hello
+  "$sbt" assembly
+fi
 
 run "Servlet" "$java" -jar "target/scala-2.11/hello-assembly-0.0.0.jar"
 run "Node.js" "$node"  hello.js
